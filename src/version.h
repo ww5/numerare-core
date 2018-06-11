2ee4bfecae275429cf88e8fbc66c1e3e4933b620
numerare <38866023+numerare@users.noreply.github.com>
2018-06-11 22:49:14 +0200

    reset reposity

--- src/version.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-extern const char* const NUMERARE_BUILD;
-extern const char* const NUMERARE_VERSION;
-extern const char* const NUMERARE_RELEASE_NAME;
-extern const char* const NUMERARE_STRING_VERSION;
